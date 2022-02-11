db07d176760d2117f687133e61def7d4f7e9440c
Russell Waring <russell.waring@dcmail.ca>
2022-02-11 00:55:41 -0500

    added 'skeletons' of javascript page functions

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,12 +1,75 @@
 //IIFE -- Imediately invoked function expression
 // aka - anonymously self executing function
+/** app.js
+ *  @authors    Katherine Bellman, Russell Waring
+ *  @since      2022.02.07
+ *  @version    1
+ *              JavaScript file for injecting behaviour into our website.
+ */
+// IIFE -- Immediately Invoked Function Expression
+// AKA -- Anonymous Self-Executing Function
 (function()
 {
-    function Start()
+
+    function DisplayHomePage()
     {
-        console.log("App Started!");
+        // Name of button and id are same... this is a naming convention. Button is actually an 'html element'
+        console.log("Home Page");
+        let AboutUsButton = document.getElementById("AboutUsButton");
+        AboutUsButton.addEventListener("click", function()
+        {
+            location.href = "about.html";
+        });
     }
 
-    window.addEventListener("load", Start);
+    function DisplayProductsPage()
+    {
+        console.log("Products Page");
+    }
 
-}())();
+    function DisplayServicesPage()
+    {
+        console.log("Services Page");
+    }
+
+    function DisplayAboutPage()
+    {
+        console.log("About Page");
+    }
+
+    function DisplayContactPage()
+    {
+        console.log("Contact Page");
+    }
+
+
+    // named function option
+    function Start() 
+    {
+        console.log("App Started!");
+
+        switch(document.title)
+        {
+            case "Home":
+                DisplayHomePage();
+                break;
+            case "Products":
+                DisplayProductsPage();
+                break;
+            case "Services":
+                DisplayServicesPage();
+                break;
+            case "About Us":
+                DisplayAboutPage();
+                break;
+            case "Contact Us":
+                DisplayContactPage();
+                break;
+        }        
+    }
+
+    // Event listener for hooking into the Start function
+    window.addEventListener("load", Start); 
+
+
+})(); 
